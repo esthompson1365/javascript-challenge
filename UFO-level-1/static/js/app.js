ed14ddacc82f3014493a0d7d2784b89dc4821615
Ed <esthompson@hotmail.com>
2020-01-16 21:34:15 -0500

    Part 1 complete!

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,23 +1,49 @@
+function renderTable(dataSet) {
+    // get a reference to the tbody tag
+    var tbody = d3.select('tbody');
+    
+    // Find all child <tr> and delete them, emptying the table
+    tbody.selectAll('tr').remove()
+
+    // Loop through the data
+    dataSet.forEach((dataPoint) => {
+        // add a <tr>
+        var tr = tbody.append('tr');
+
+        // add <td> tags for table data fields
+        tr.append('td').text(dataPoint.datetime);
+        tr.append('td').text(dataPoint.city);
+        tr.append('td').text(dataPoint.state);
+        tr.append('td').text(dataPoint.country);
+        tr.append('td').text(dataPoint.shape);
+        tr.append('td').text(dataPoint.durationMinutes);
+        tr.append('td').text(dataPoint.comments);
+    });
+}
+
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
 
-// get a reference to the tbody tag
-var tbody = d3.select('tbody');
+renderTable(tableData)
 
-// Loop through the data
-data.forEach((dataPoint) => {
-    // add a <tr>
-    var tr = tbody.append('tr');
+// Reference to the filter button
+var filterButton = d3.select('button#filter-btn')
 
-    // add <td> tags for table data fields
-    tr.append('td').text(dataPoint.datetime);
-    tr.append('td').text(dataPoint.city);
-    tr.append('td').text(dataPoint.state);
-    tr.append('td').text(dataPoint.country);
-    tr.append('td').text(dataPoint.shape);
-    tr.append('td').text(dataPoint.durationMinutes);
-    tr.append('td').text(dataPoint.comments);
+// Listen for click events, and filter the data
+filterButton.on('click', () => {
+    // get date from input
+    var dateTimeInput = d3.select('input#datetime').property('value')
+    
+    // filter the data set (non-permanently)
+    var filteredData = tableData.filter(dataPoint => dataPoint.datetime.startsWith(dateTimeInput))
+
+    // render the filtered data
+    renderTable(filteredData)
+
 });
+
+
 
