fa4835ce6cb54ae470946331c0296865cfda470a
Ed <esthompson@hotmail.com>
2020-01-16 20:50:26 -0500

    We built a table!

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,22 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// get a reference to the tbody tag
+var tbody = d3.select('tbody');
+
+// Loop through the data
+data.forEach((dataPoint) => {
+    // add a <tr>
+    var tr = tbody.append('tr');
+
+    // add <td> tags for table data fields
+    tr.append('td').text(dataPoint.datetime);
+    tr.append('td').text(dataPoint.city);
+    tr.append('td').text(dataPoint.state);
+    tr.append('td').text(dataPoint.country);
+    tr.append('td').text(dataPoint.shape);
+    tr.append('td').text(dataPoint.durationMinutes);
+    tr.append('td').text(dataPoint.comments);
+});
+
